2ce99fd814792b24b7ace613898eb505ae761330
dotNETDev-Zayniddin <dotnetdev2000@gmail.com>
2024-10-03 00:25:18 +0500

    INTRO: Upload Project

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Calculatr{
+    public class Program{
+        static void Main(string[] args){
+            System.Console.Write("Enter a first number: ");
+            string tempfirstNumber = Console.ReadLine();
+            int firstNumber = Convert.ToInt32(tempfirstNumber);
+            System.Console.Write("Enter the second one: ");
+            string tempsecondNumber = Console.ReadLine();
+            int secondNumber = Convert.ToInt32(tempsecondNumber);
+            System.Console.WriteLine($"First number is greater than 2nd number: {firstNumber > secondNumber}");
+            System.Console.WriteLine($"1st number is greater than or equlat to 2nd number: {firstNumber >= secondNumber}");
+            System.Console.WriteLine($"1st number less than 2nd number: {firstNumber < secondNumber}");
+            System.Console.WriteLine($"1st number less or equal to 2nd number: {firstNumber <= secondNumber}");
+            System.Console.WriteLine($"Numbers are equal: {firstNumber == secondNumber}");
+            System.Console.WriteLine($"Numbers are not equal: {firstNumber != secondNumber}");
+
+        }
+    }
+}
